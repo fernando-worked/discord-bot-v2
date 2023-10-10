664484954736920843637b4013e152bb19895c48
fernandomoreira <fernando.worked@gmail.com>
2023-10-09 23:15:45 -0300

    incluido todo

---
+++ src/discord/components/relatorio/buttons/cancelar.ts
@@ -0,0 +1,18 @@
+import { Component } from "@/discord/base";
+import { valoresSelectUsuarioRelatorio } from "../selects/membros";
+import { mapInteracaoRelatorio } from "@/discord/commands/relatorio";
+
+new Component({
+    customId: "btn_cancelar_relatorio",
+    type: "Button", cache: "cached",
+    async run(interaction) {
+
+        mapInteracaoRelatorio.get(interaction.user.id)!.forEach(async (elemento, index) => {
+            elemento.delete();
+            mapInteracaoRelatorio.get(interaction.user.id)!.splice(index, 1);
+        });
+
+        valoresSelectUsuarioRelatorio.delete(interaction.user.id);
+
+    },
+});
