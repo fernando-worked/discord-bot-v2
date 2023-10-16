c63a359da600367ea4a750425ed8b5ae2e79269c
fernando-worked <fernando.worked@gmail.com>
2023-10-16 16:52:01 -0300

    novas implementacoes onJoin

--- src/discord/events/member/onJoin.ts
+++ src/discord/events/member/onJoin.ts
@@ -5,6 +5,10 @@ new Event({
     run(member) {
         const canalGeral = member.guild.channels.cache.get(Canais.GERAL); // Supondo que Canais.GERAL contenha o ID do canal geral
 
+        /* Melhorar formato para EMBED */
+        if(canalGeral && canalGeral.isTextBased())
+        canalGeral.send({content: `Olá ${member}! Seja bem vindo ao servidor SWAT BR, um servidor focado em roleplay. Leia nossas regras e divirta-se!`});
+
         for(let cargo in CargosIniciais){
             const role = member.guild.roles.cache.get(cargo);
             if(role) member.roles.add(role);
