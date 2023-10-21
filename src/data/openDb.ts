ba2c1fc077d75eee920aa4c81c357bb5866c0e3e
fernandomoreira <fernando.worked@gmail.com>
2023-10-20 22:56:33 -0300

    modificado todo pendencias

--- src/data/openDb.ts
+++ src/data/openDb.ts
@@ -3,7 +3,7 @@ import { open } from "sqlite"
 
 export async function openDb () {
   return open({
-    filename: `${process.env.NODE_ENV == "producao" ? "" : "src/"}data/db/database.db`,
+    filename: `database.db`,
     driver: sqlite3.Database
   });
 }
